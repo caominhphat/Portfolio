7b9fc010f91e1864581612fe2588183d5a6a31e2
caominhphat <you@example.comcaophat2713@gmail.com>
2021-10-14 22:47:55 +0700

    14/10 update

--- js/script.js
+++ js/script.js
@@ -1,125 +1,191 @@
 $(document).ready(function () {
-
-  $('header li').click(function (e) { 
+  $("header li").click(function (e) {
     e.preventDefault();
-    $(this).addClass('active');
-    $(this).siblings('header li.active').removeClass('active');
-    
+    $(this).addClass("active");
+    $(this).siblings("header li.active").removeClass("active");
   });
-    $(".back-to-top").click(function(event) {
-        /* Act on the event */
-        // đơn vị thời gian là ms
-        // button back to top so với top
-        //Lấy top của button back to top
-        //offset() là vị trí của phần tử so với document
-        //position() là vị trí của phần tử so với cha trực tiếp 
-        var distance = $(this).offset().top;
-        var v = 2; //2px/ms
-        var time = distance / v;
-        $("html").animate({scrollTop: 0}, time);
-    });
-    var count = 0
-    // Đoạn lệnh này dùng để làm ẩn và hiện menu hoặc nut bakctotop
-    $(window).scroll(function(event) {
-        /* Act on the event */
-        
-      count += 1000
-        // return the position of scroll bar compared to top of viewport
-        var windowTop = $(this).scrollTop();  
-        $('header').css({'position': 'fixed','top': '2vh'});     
-        
-	    if (windowTop >= $("#about").offset().top - 1) {
-		    $('li>a').css('color','#425e82')
-        $('li div').css('background-color',"#425e82")
-    	}
-      else{
-        $('li>a').css('color','white')
-        $('li div').css('background-color',"white")
+ $('.back-to-top').click(function (e) { 
+   e.preventDefault();
+    index = 0;
+   var home = document.getElementById('home')
+   home.scrollIntoView({behavior:'smooth'})
+   setTimeout(()=>{
+     window.location.hash = '#home'
+   },500)
+
+   li.forEach((el,index)=>{
+     if(el.className.includes('active')){
+       el.classList.remove('active')
+    }
+      if(el.children[0].innerHTML == 'HOME'){
+        el.classList.add('active')
       }
+   })
+   $("li>a").css("color", "white");
+   $("li div").css("background-color", "white");
 
-      var li = $('ul.nav-bar li')
-      var section = $('section')
-      // if(windowTop >= $("#home").offset().top && windowTop < $("#about").offset().top - 1){
-      //   $("#about").animate({scrollTop: $("#about").offset().top}, 1000, updateHashTag(hashTag));
-      // }
+ });
+  var count = 0;
+  // Đoạn lệnh này dùng để làm ẩn và hiện menu hoặc nut bakctotop
+  // $(window).scroll(function (event) {
+  //   /* Act on the event */
 
-      // if(windowTop >= 0 && windowTop < 969){
-      //   $("html").animate({scrollTop: $("#about").offset().top}, 1000);
-      // }
-      // if(windowTop >= 969 && windowTop < (969+969) && currentWindowTop > windowTop){
-      //   $("html").animate({scrollTop: $("#language").offset().top}, 1000);
-      // }
-      
-      // $("html").animate({scrollTop: count}, 1000,function(){
-      //   $("html").stop()
-      // } );
+  //   count += 1000;
+  //   // return the position of scroll bar compared to top of viewport
+  //   var windowTop = $(this).scrollTop();
+  //   $("header").css({ position: "fixed", top: "2vh" });
 
-        toggleBackToTop();
-    });
+  //   if (windowTop >= $("#about").offset().top - 1) {
+  //     $("li>a").css("color", "#425e82");
+  //     $("li div").css("background-color", "#425e82");
+  //   } else {
+  //     $("li>a").css("color", "white");
+  //     $("li div").css("background-color", "white");
+  //   }
 
-    $("#toggle").click(function (e) { 
-        e.preventDefault();
-        $('nav').slideToggle();
-    });
+  //   var li = $("ul.nav-bar li");
+  //   var section = $("section");
+  //   // if(windowTop >= $("#home").offset().top && windowTop < $("#about").offset().top - 1){
+  //   //   $("#about").animate({scrollTop: $("#about").offset().top}, 1000, updateHashTag(hashTag));
+  //   // }
 
-    $("header li a").click(function(event) {
-        /* Act on the event */
-        //Ngăn chặn không cho chạy đến vùng có id tương với hash tag của thẻ a
-        // event.preventDefault();
-        var hashTag = this.hash;//javascript
-      
-        
-        var targetObj = $(hashTag);
-        
-        var targetTop = $(targetObj).offset().top;
-    
-        var currentWindowTop = $(window).scrollTop();
-        var distance = Math.abs(currentWindowTop - targetTop);
-       
-        var v = 1; 
-        var time = distance / v;
-    
-        $("html").animate({scrollTop: targetTop}, 1000, function () { updateHashTag(hashTag) });
-    
-    });
-    let number = document.getElementsByClassName('number');
-    
-    let counter = 0;
-    setInterval(()=>{
-      if(counter == 60){
-        clearInterval();
-      }else{
-       counter+=1;
-       for(let i = 0; i< number.length;i++){
-        number[i].innerHTML = counter +"%";
-       }   
-      }
-      
-    },2000/60)
+  //   // if(windowTop >= 0 && windowTop < 969){
+  //   //   $("html").animate({scrollTop: $("#about").offset().top}, 1000);
+  //   // }
+  //   // if(windowTop >= 969 && windowTop < (969+969) && currentWindowTop > windowTop){
+  //   //   $("html").animate({scrollTop: $("#language").offset().top}, 1000);
+  //   // }
 
-  
+  //   // $("html").animate({scrollTop: count}, 1000,function(){
+  //   //   $("html").stop()
+  //   // } );
 
-    $('.language .slick-prev').html('<i class="fas fa-chevron-left"></i>');
-    $('.language .slick-next').html('<i class="fas fa-chevron-right"></i>');
+  //   toggleBackToTop();
+  // });
+
+  $("#toggle").click(function (e) {
+    e.preventDefault();
+    $("nav").slideToggle();
+  });
+
+  $("header li a").click(function (event) {
+
+    event.preventDefault();
    
- 
+    var sections = document.querySelectorAll('section')
+    sections.forEach((section,i)=>{
+      if(this.innerHTML.toLowerCase() == section.getAttribute('id')){
+        index = i
+        console.log(i)
+        section.scrollIntoView({behavior:'smooth'})
+        const hashTag = this.getAttribute('href')
+        setTimeout(function () {
+          updateHashTag(hashTag);
+        }, animationDuration / 2);
+
+        if(section.getAttribute('id') != 'home'){
+          $("li>a").css("color", "#425e82");
+          $("li div").css("background-color", "#425e82");
+        }else{
+          $("li>a").css("color", "white");
+          $("li div").css("background-color", "white");
+        }
+      }
+    })
+
+  });
+  let number = document.getElementsByClassName("number");
+
+  let counter = 0;
+  setInterval(() => {
+    if (counter == 60) {
+      clearInterval();
+    } else {
+      counter += 1;
+      for (let i = 0; i < number.length; i++) {
+        number[i].innerHTML = counter + "%";
+      }
+    }
+  }, 2000 / 60);
+
+  $(".language .slick-prev").html('<i class="fas fa-chevron-left"></i>');
+  $(".language .slick-next").html('<i class="fas fa-chevron-right"></i>');
+
+  const sections = document.querySelectorAll("section");
+  const li = document.querySelectorAll(".nav-item");
+  let index = 0;
+  let lastTime = 0;
+  const animationDuration = 1000;
+
+  document.addEventListener("wheel", (event) => {
+    var delta = event.wheelDelta;
+    const currentTime = new Date().getTime();
+    if (currentTime - lastTime < animationDuration) {
+      e.preventDefault();
+      return;
+    }
+    if (delta < 0) {
+      if (index > 3) return;
+      index++;
+      sections.forEach((section, i) => {
+
+        if (i == index) {
+          section.scrollIntoView({ behavior: "smooth" });
+          if (i != 0) {
+            $("li>a").css("color", "#425e82");
+            $("li div").css("background-color", "#425e82");
+          }
+
+          li.forEach((el, j) => {
+            if (el.className.includes("active")) {
+              el.classList.remove("active");
+            }
+            if (
+              el.children[0].innerHTML.toLowerCase() ==
+              section.getAttribute("id")
+            ) {
+              el.classList.add("active");
+              setTimeout(function () {
+                updateHashTag(el.children[0].getAttribute("href"));
+              }, animationDuration / 2);
+            }
+          });
+        }
+      });
+    } else {
+      if (index < 1) return;
+      index--;
+      sections.forEach((section, i) => {
+        if (i == index) {
+          section.scrollIntoView({ behavior: "smooth" });
+          if (i == 0) {
+            $("li>a").css("color", "white");
+            $("li div").css("background-color", "white");
+          }
+          li.forEach((el, j) => {
+            if (el.className.includes("active")) {
+              el.classList.remove("active");
+            }
+            if (
+              el.children[0].innerHTML.toLowerCase() ==
+              section.getAttribute("id")
+            ) {
+              el.classList.add("active");
+
+              setTimeout(function () {
+                updateHashTag(el.children[0].getAttribute("href"));
+              }, animationDuration / 2);
+            }
+          });
+        }
+      });
+    }
+    lastTime = currentTime;
+  });
 });
 function updateHashTag(hashTag) {
-	window.location.hash = hashTag;
+  window.location.hash = hashTag;
 }
 function toggleBackToTop() {
-	windowTop = $(window).scrollTop(); 
-	if (windowTop < 10) {
-		//Ẩn nút back-to-top
-		$(".back-to-top").fadeOut();
-	}
-	else {
-		//Hiện ra nút back-to-top
-		$(".back-to-top").fadeIn();
-	}
+ 
 }
-
-
-
-
-
